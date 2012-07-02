c110adf82f782bf0bd9edb365c7d9ae653e1a147
PiVolan <pivolan@gmail.com>
2012-07-02 23:45:55 +0400

    парсер видео

--- UrlParser.php
+++ UrlParser.php
@@ -9,28 +9,132 @@ namespace parser;
  */
 class UrlParser
 {
-	static public function urlParser($url)
+	/**
+	 * Функция парсит сыслку на видеопортал, и вытягивает оттуда описание видео, название и сам код для вставки.
+	 * youtube smotri.com mailru rutube и другие.
+	 *
+	 * @static
+	 *
+	 * @param     $url
+	 * @param int $width  ширина видео вставляемого в код
+	 * @param int $height высота
+	 *
+	 * @return array|bool
+	 */
+	public static function videoParser($url, $width = 320, $height = 240)
 	{
-		if (empty($url))
-		{
+		if (empty($url)) {
+			return false;
+		}
+		$pageHtml = self::getPage($url, $info);
+		if ($info["http_code"] != 200 && $info["http_code"] != 404) {
+			return false;
+		}
+		$headerCharset = array();
+		preg_match('/charset=(?P<charset>.+)/', $info['content_type'], $headerCharset);
+		$charset = 'utf-8';
+		if (isset($headerCharset['charset'])) {
+			$charset = trim(strtolower($headerCharset['charset']));
+		}
+		else {
+			$matchesCharset = array();
+			preg_match('/<meta [^"]*"content-type" content="[^=]+=(?P<charset>[^"]+)"/mi', $pageHtml, $matchesCharset);
+			if (isset($matchesCharset['charset'])) {
+				$charset = trim(strtolower($matchesCharset['charset']));
+			}
+		}
+//		$pageHtmlTest = $pageHtml;
+//		$winPageHtml = iconv($charset, 'Windows-1251//TRANSLIT//IGNORE', $pageHtml);
+//		$pageHtml = iconv('Windows-1251', 'UTF-8//IGNORE', $winPageHtml);
+//		$winPageHtml = mb_convert_encoding($pageHtml, 'Windows-1251','auto');
+//		$pageHtml = mb_convert_encoding($winPageHtml , 'UTF-8','Windows-1251');
+//		$pageHtml = iconv($charset, 'UTF-8', $pageHtml);
+		if ($charset != 'utf-8') {
+			$pageHtml = iconv($charset, 'UTF-8', $pageHtml);
+		}
+		$matches = array();
+		$match = array(
+			'thumbnail'  => '',
+			'url'        => '',
+			'id'         => '',
+			'http'       => '',
+			'host'       => $info['url'],
+			'title'      => '',
+			'description'=> '',
+			'video_src'  => '',
+			'height'     => $height,
+			'width'      => $width,
+		);
+		preg_match('/\<meta (name="title"|property="og:title") content="?([^"]*)"/s', $pageHtml, $matches['title']);
+		preg_match('/\<h1 class="mb20"\>?(.*)\<\/h1\>/', $pageHtml, $matches['mailru_title']);
+		//preg_match('/\<h1\>Описание:\<\/h1.\>?(.*)/', $pageHtml, $matches['mailru_description']);
+		//preg_match('/\<meta property="og:title" content="(?P<title>.*)"/si', $pageHtml, $matches[1]);
+
+		preg_match('/\<meta (name="description"|property="og:description") content="?([^"]*)"/s', $pageHtml, $matches['description']);
+		preg_match('/(link rel="video_src" href=|meta property="og:video" content=)"?(.*)"/', $pageHtml, $matches['video_src']);
+		preg_match('/(link rel="image_src" href=|meta property="og:image" content=)"?(.*)"/', $pageHtml, $matches['image_src']);
+
+		preg_match('/smotri\.com.*(id|file)=(.*)/', $url, $matches['url_video_smotri_com']);
+		if (isset($matches['mailru_description'][1]) && !empty($matches['mailru_description'][1])) {
+			$match['description'] = html_entity_decode($matches['mailru_description'][1]);
+		}
+		elseif (isset($matches['description'][2])) {
+			$match['description'] = html_entity_decode($matches['description'][2]);
+		}
+		// title
+		if (isset($matches['mailru_title'][1]) && !empty($matches['mailru_title'][1])) {
+			$match['title'] = html_entity_decode($matches['mailru_title'][1]);
+		}
+		elseif (isset($matches['title'][2])) {
+			$match['title'] = html_entity_decode($matches['title'][2]);
+		}
+		// video_src
+		if (isset($matches['video_src'][2]) && !empty($matches['video_src'][2])) {
+			$match['url'] = '<embed src="' . $matches['video_src'][2] . '" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true" width="' . $width . '" height="' . $height . '"></embed>';
+			$match['video_src'] = $matches['video_src'][2];
+		}
+		elseif (isset($matches['url_video_smotri_com'][2])) {
+			$match['url'] = '<embed src="http://pics.smotri.com/player.swf?xmlsource=http%3A%2F%2Fpics.smotri.com%2Fcskins%2Fblue%2Fskin_color.xml&xmldatasource=http%3A%2F%2Fpics.smotri.com%2Fskin_ng.xml&file=' . $matches['url_video_smotri_com'][2] . '" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true" width="' . $width . '" height="' . $height . '"></embed>';
+			$match['video_src'] = 'http://pics.smotri.com/player.swf?xmlsource=http%3A%2F%2Fpics.smotri.com%2Fcskins%2Fblue%2Fskin_color.xml&xmldatasource=http%3A%2F%2Fpics.smotri.com%2Fskin_ng.xml&file=' . $matches['url_video_smotri_com'][2];
+		}
+		else {
+			return false;
+		}
+		// image_src
+		if (isset($matches['image_src'][2]) && !empty($matches['image_src'][2])) {
+			$match['thumbnail'] = $matches['image_src'][2];
+		}
+		$match['description'] = mb_convert_encoding($match['description'], 'UTF-8', 'auto');
+		$match['title'] = mb_convert_encoding($match['title'], 'UTF-8', 'auto');
+		return $match;
+	}
+
+	/**
+	 * Парсинг сайтов. Вытягивает title. И находит первые тексты для описания.
+	 *Автоматически конвертирует кодировку, если она не равна utf-8
+	 * @static
+	 *
+	 * @param $url
+	 *
+	 * @return array|bool
+	 */
+	static public function siteParser($url)
+	{
+		if (empty($url)) {
 			return array();
 		}
 		$curl = self::getPage($url, $info);
-		if ($info["http_code"] == 200)
-		{
+		if ($info["http_code"] == 200) {
 			$headerCharset = array();
 			preg_match('/charset=(?P<charset>.+)/', $info['content_type'], $headerCharset);
 			$charset = 'utf-8';
-			if (isset($headerCharset['charset']))
-			{
+			if (isset($headerCharset['charset'])) {
 				$charset = trim(strtolower($headerCharset['charset']));
 			}
-			else
-			{
+			else {
 				$matchesCharset = array();
 				preg_match('/<meta [^"]*"content-type" content="[^=]+=(?P<charset>[^"]+)"/mi', $curl, $matchesCharset);
-				if (isset($matchesCharset['charset']))
-				{
+				if (isset($matchesCharset['charset'])) {
 					$charset = trim(strtolower($matchesCharset['charset']));
 				}
 			}
@@ -38,46 +142,37 @@ class UrlParser
 			preg_match('/<meta name="description" content="(?P<text>[^"]+)"/mi', $curl, $matchesDescr);
 			$result = array('title' => '', 'description' => '');
 
-			if (isset($matches['text']))
-			{
-				if ($charset != 'utf-8')
-				{
+			if (isset($matches['text'])) {
+				if ($charset != 'utf-8') {
 					$result['title'] = iconv($charset, 'UTF-8', $matches['text']);
 				}
-				else
-				{
+				else {
 					$result['title'] = $matches['text'];
 				}
 				$result['title'] = htmlspecialchars_decode($result['title']);
 			}
-			if (isset($matchesDescr['text']))
-			{
-				if ($charset != 'utf-8')
-				{
+			if (isset($matchesDescr['text'])) {
+				if ($charset != 'utf-8') {
 					$result['description'] = iconv($charset, 'UTF-8', $matchesDescr['text']);
 				}
-				else
-				{
+				else {
 					$result['description'] = $matchesDescr['text'];
 				}
 				$result['description'] = htmlspecialchars_decode($result['description']);
 			}
 
 			$parse = strpos($url, '://');
-			if ($parse === false)
-			{
+			if ($parse === false) {
 				$link['urloriginal'] = 'http://' . $url;
 			}
-			else
-			{
+			else {
 				$link['urloriginal'] = $url;
 			}
 
 			$result['url'] = parse_url($link['urloriginal']);
 			$result['urloriginal'] = $link['urloriginal'];
 		}
-		else
-		{
+		else {
 			$result = false;
 		}
 		return $result;
